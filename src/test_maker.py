893bd83d4f4b77c0e35b878eb33677d9cb005064
eduardodpms <eduardopms19@hotmail.com>
2025-10-17 23:50:56 -0300

    style: melhorar interface do test maker

--- src/test_maker.py
+++ src/test_maker.py
@@ -2,12 +2,13 @@ import json
 
 def test_maker(json_path):
     while True:
+        print(">>> New test case:")
         ID = input("id (string): ")
         T = input("title (string): ")
         C = input("body (string): ")
         F = input("source (string): ")
         P = input("sponsored (bool): ")
-        E = input("expected output (string): ")
+        E = input("expected output (string - empty if valid class): ")
 
         new_test = {
             "id": ID,
@@ -26,4 +27,4 @@ def test_maker(json_path):
         with open(json_path, 'w') as json_file:
             json.dump(tests, json_file, indent=4)
 
-        print(f"\nNew test case saved: ID = {ID}\n{'='*15} Ctrl+C to exit {'='*15}\n")
+        print(f"\n>>> New test case saved: ID = {ID}\n\n{'='*15} Ctrl+C to exit {'='*15}\n")
